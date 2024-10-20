64b8bd5b3c012dad6809c873a0260ee511e13840
Laryssa <0000111139054xsp@al.educacao.sp.gov.br>
2024-10-20 16:22:04 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,54 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em inglês?',
+    'Oi em inglês é HI (RAI)'
+)
+
+criaCartao(
+    'Astronomia',
+    'Qual a estrela mais próxima do planeta Terra depois do Sol?',
+    'Proxima Centauri'
+)
+
+criaCartao(
+    'Matemática',
+    'Como se calcula a área de um circulo?',
+    'Para calcular a área de um circulo basta utilizar o cálculo  A = π r² (área igual a pi vezes r ao quadrado)'
+)
+
+criaCartao(
+    'Sociologia',
+    'Qual o nome do grupo responsável pelo movimento social contra a violência policial e a opressão racial enfrentada pelos negros nos Estados Unidos em 1966?',
+    'Black Panthers (em português, Os Panteras Negras)'
+)
+
+criaCartao(
+    'História',
+    'Qual o nome do primeiro presidente pós-ditadura do Brasil?',
+    'Após a morte de Tancredo Neves, antes de sua posse, José Sarney assume a presidência do pais'
+)
+
+criaCartao(
+    'Filosofia',
+    'Quem era o filósofo que zombou de Alexandre o Grande e de uma das teorias de Platão?',
+    'Diógenes de Sinope, também conhecido como Diógenes, o Cínico'
+)
+
